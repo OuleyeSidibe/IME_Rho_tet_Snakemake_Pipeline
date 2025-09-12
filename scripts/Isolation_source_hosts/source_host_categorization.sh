5d2a699be97c0c18d379d61f9a38c21b013adbc8
OuleyeSidibe <142305518+OuleyeSidibe@users.noreply.github.com>
2025-09-12 15:43:43 +0200

    Update source_host_categorization.sh

--- scripts/Isolation_source_hosts/source_host_categorization.sh
+++ scripts/Isolation_source_hosts/source_host_categorization.sh
@@ -2,10 +2,10 @@
 # Extract source and host metadata for the ~65K genomes of the original dataset
 
 # 1. on MIGALE -> extract metadata for each genome
-./collect_sourcehost_forall > sourcehost.out 2> missing_assemblies.out
+./1_collect_sourcehost_forall.sh > sourcehost.out 2> missing_assemblies.out
 
 # 2. categorize according to identified keywords
-cat sourcehost.out| ./categorize > sourcehost.categorized.out
+cat sourcehost.out| ./2_categorize.sh > sourcehost.categorized.out
 
 # 3. return distribution gut / non gut / unknown
 cat sourcehost.categorized.out | cut -f 3 | sort | uniq -c
@@ -21,7 +21,7 @@ cat sourcehost.categorized.out | grep -P "\tgut" | cut -f 4 | sort | uniq -c
 cat  ../RefseqFINAL5_TIRS_IME_Rho_tet.csv | awk 'BEGIN{FS=","}{print $6"\t"$7"\t"$1 }'> IME_Rho_tet_sourcehost.out
 
 # 2. categorize according to identified keywords
-cat IME_Rho_tet_sourcehost.out| ./categorize > IME_Rho_tet_sourcehost.categorized.out
+cat IME_Rho_tet_sourcehost.out| ./2_categorize.sh > IME_Rho_tet_sourcehost.categorized.out
 
 # 3. Merge manually the source_host and the original genome table. 
 
@@ -33,7 +33,7 @@ cat IME_Rho_tet_sourcehost.out| ./categorize > IME_Rho_tet_sourcehost.categorize
 cat  ../RefseqFINAL5_TIRS_tet.csv | awk 'BEGIN{FS=","}{print $6"\t"$7"\t"$1 }'> tet_sourcehost.out
 
 # 2. categorize according to identified keywords
-cat tet_sourcehost.out| ./categorize > tet_sourcehost.categorized.out
+cat tet_sourcehost.out| ./2_categorize.sh > tet_sourcehost.categorized.out
 
 # 3. Merge manually the source_host and the original genome table. 
 
